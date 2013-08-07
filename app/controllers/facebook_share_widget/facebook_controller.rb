80d44deeccea83e6c2389d7376c4a636626d31aa
Nic Snoek <nicsnoek@yahoo.com>
2013-08-07 15:16:52 +1000

    It's not an error when you are not logged in, you just can not see anything...

--- app/controllers/facebook_share_widget/facebook_controller.rb
+++ app/controllers/facebook_share_widget/facebook_controller.rb
@@ -1,11 +1,11 @@
 class FacebookShareWidget::FacebookController < FacebookShareWidget::ApplicationController
   include FacebookShareWidget::FacebookHelper
-  
+
   before_filter :sanitize_params, :except => [:share]
 
   def index
   end
-  
+
   def personal_data
     begin
       @personal_data_results = my_personal_data @personal_data_type
@@ -34,20 +34,39 @@ class FacebookShareWidget::FacebookController < FacebookShareWidget::Application
 
   def friends
     begin
-      render json: facebook_friends_for_link(params[:link], @personal_dataId, @personal_data_type), status: :ok
+      render json: get_friends, status: :ok
     rescue Exception => ex
       log_exception_and_render_as_json(ex)
     end
   end
-  
+
+  def get_friends
+    handle_fb_graph_exceptions([]) do
+      facebook_friends_for_link(params[:link], @personal_dataId, @personal_data_type)
+    end
+  end
+
+  def handle_fb_graph_exceptions(error_default = nil)
+    begin
+      yield
+    rescue FbGraph::InvalidToken
+      session.delete(FacebookShareWidget.access_token_session_key)
+      error_default
+    rescue FbGraph::Auth::VerificationFailed
+      error_default
+    end
+  end
+
   def share
     begin
-      if params[:post_id]
-        me = facebook_me.fetch
-        share = FacebookShareWidget::Share.new(user_facebook_id: me.identifier, friend_facebook_id: params[:facebook_id], url: params[:link], message: message_for(params[:post_id]))
-        share.save!
+      handle_fb_graph_exceptions do
+        if params[:post_id]
+          me = facebook_me.fetch
+          share = FacebookShareWidget::Share.new(user_facebook_id: me.identifier, friend_facebook_id: params[:facebook_id], url: params[:link], message: message_for(params[:post_id]))
+          share.save!
+        end
+        render layout: false
       end
-      render layout: false
     rescue Exception => ex
       log_exception_and_render_as_json(ex)
     end
@@ -59,7 +78,7 @@ class FacebookShareWidget::FacebookController < FacebookShareWidget::Application
     @personal_dataId = params[:personal_dataId] ? params[:personal_dataId].to_i : nil
     @personal_data_type = params[:personal_data_type].present? ? params[:personal_data_type].gsub('$','.') : nil
   end
-  
+
   def log_exception_and_render_as_json(ex)
     Rails.logger.warn ex.message
     Rails.logger.warn ex.backtrace.join("\n")
